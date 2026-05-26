38729fcc232e2958b240f32e666d98985625c878
PE Stack Pulse <harry.sarshogh@gmail.com>
2026-05-26 23:25:22 +0100

    feat: implement tf-atom-subnet-aws module (#1)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,24 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "id" {
+  description = "ID of the subnet"
+  value       = try(aws_subnet.this[0].id, null)
+}
+
+output "arn" {
+  description = "ARN of the subnet"
+  value       = try(aws_subnet.this[0].arn, null)
+}
+
+output "cidr_block" {
+  description = "CIDR block of the subnet"
+  value       = try(aws_subnet.this[0].cidr_block, null)
+}
+
+output "availability_zone" {
+  description = "AZ of the subnet"
+  value       = try(aws_subnet.this[0].availability_zone, null)
 }
